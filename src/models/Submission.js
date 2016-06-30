fc1bc4b2c940e34d5f010fda424e48f8a9184dc0
Robert Kerr <robrkerr@gmail.com>
2016-06-30 17:29:23 +1000

    Moved prefilled of examples out into a separate file and made this depend on the url on page load.

--- src/models/Submission.js
+++ src/models/Submission.js
@@ -1,35 +1,25 @@
+import examples from './examples'
+
+const emptyData = {
+  title: '',
+  description: {
+    text: '',
+    context: {},
+    redactions: {}
+  }
+}
+
 const api = window.location.hostname.match(/localhost/) ? 'http://localhost:3000' : 'https://vsconf-cfp'
+const exampleName = window.location.href.split('?')[1]
 
 export default class Submission {
   constructor() {
-    this.data = {
-      title: '',
-      description: {
-        text: '',
-        context: {},
-        redactions: {}
-      }
-    }
-    // this.data = {
-    //   title: '',
-    //   description: {
-    //     text: 'As part of my work for COMPANY_A, I am developing PROJECT_A and we are doing some amazing things with Threejs. Building on what PERSON_A built, we show that it is possible to...',
-    //     context: { 
-    //       COMPANY_A: 'Recent startup that will only be familar to a few people.',
-    //       PROJECT_A: 'New project that would not have been spoken about in a conference talk.',
-    //       PERSON_A: 'Well known and respected Javascript developer.'
-    //     },
-    //     redactions: {
-    //       COMPANY_A: 'FireBoat',
-    //       PROJECT_A: 'lol.js',
-    //       PERSON_A: 'Smithy'
-    //     }
-    //   }
-    // }
+    this.data = examples[exampleName] || emptyData
+    this.example = examples[exampleName] !== undefined
   }
 
   submit() {
-    // console.log(JSON.stringify(this.data));
+    console.log(JSON.stringify(this.data))
     return fetch(`${api}/proposals`, {
       method: 'POST',
       headers: {
