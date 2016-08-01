01d21f35173940b5633c1b9e1d04a478ad3d1e96
Glen Maddern <glenmaddern@gmail.com>
2016-08-01 14:38:47 +1000

    looks good, 10pm tonight it goes live

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './components/App'
+import AllOver from './components/AllOver'
 import './style/style'
 import examples from './models/examples'
 import api from './utils/api'
@@ -14,6 +15,12 @@ const conference = window.location.hostname.match(/css/) ? {
   title: 'JSConf AU 2016',
   url: 'http://2016.jsconfau.com'
 }
+
+ReactDOM.render(
+  <AllOver conference={conference}/>,
+  document.getElementById('app')
+)
+/*
 
 const lookup = window.location.search.match(/^\?(example(\d+)|([0-9a-f]{24}))$/)
 const fetchExisting = new Promise((resolve, reject) => {
@@ -37,3 +44,4 @@ fetchExisting.then(prefill => ReactDOM.render(
   <App conference={conference} prefill={prefill}/>,
   document.getElementById('app')
 ))
+*/
